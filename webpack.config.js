c3d51ff87b6b1700139a4cc332ada69778345693
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-19 09:12:46 +0100

    configured html files

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,12 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
       }
     ]
   }
